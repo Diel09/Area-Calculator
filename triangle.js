eb28cbe598e7c8496205eb21b634fa2ab17ef54f
Diel <72374899+Diel09@users.noreply.github.com>
2023-11-28 11:39:27 +0800

    Delete triangle.js
    Delete JS

--- triangle.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-    const { base, height } = JSON.parse(event.body);
-    const area = 0.5 * base * height;
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ shape: 'triangle', area }),
-    };
-};
