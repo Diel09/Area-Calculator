ed0970dd5cdac2db64347534d6fedb26f3d50157
Diel09 <eliezer14galano@gmail.com>
2023-11-28 14:02:07 +0800

    updated api

--- functions/app.js
+++ functions/app.js
@@ -5,14 +5,14 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Calculate area of a square
-app.post('/square', (req, res) => {
+app.post('/.netlify/functions/square', (req, res) => {
     const side = parseFloat(req.body.side);
     const area = Math.pow(side, 2);
     res.json({ shape: 'square', area });
 });
 
 // Calculate area of a rectangle
-app.post('/rectangle', (req, res) => {
+app.post('/.netlify/functions/rectangle', (req, res) => {
     const length = parseFloat(req.body.length);
     const width = parseFloat(req.body.width);
     const area = length * width;
@@ -20,20 +20,20 @@ app.post('/rectangle', (req, res) => {
 });
 
 // Calculate area of a circle
-app.post('/circle', (req, res) => {
+app.post('/.netlify/functions/circle', (req, res) => {
     const radius = parseFloat(req.body.radius);
     const area = Math.PI * Math.pow(radius, 2);
     res.json({ shape: 'circle', area });
 });
 
 // Calculate area of a triangle
-app.post('/triangle', (req, res) => {
+app.post('/.netlify/functions/triangle', (req, res) => {
     const base = parseFloat(req.body.base);
     const height = parseFloat(req.body.height);
     const area = 0.5 * base * height;
     res.json({ shape: 'triangle', area });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// Export the Express app and the handler separately
+exports.app = app;
+exports.handler = serverless(app, { binary: ['*/*'] });
