d13865281368636acc22033619e559bd45db381b
Diel <72374899+Diel09@users.noreply.github.com>
2023-11-28 11:40:15 +0800

    Delete circle.js
    Delete circle.js

--- circle.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-    const { radius } = JSON.parse(event.body);
-    const area = Math.PI * Math.pow(radius, 2);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ shape: 'circle', area }),
-    };
-};
