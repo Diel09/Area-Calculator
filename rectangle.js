1fdd6b0a67de000000928839fe96d8fcc0dd81f0
Diel <72374899+Diel09@users.noreply.github.com>
2023-11-28 11:39:53 +0800

    Delete rectangle.js
    Delete rectangle.js

--- rectangle.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-    const { length, width } = JSON.parse(event.body);
-    const area = length * width;
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ shape: 'rectangle', area }),
-    };
-};
