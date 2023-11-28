60d7120804da6d6c2dc76c989ed0dd6f96cd6ac2
Diel <72374899+Diel09@users.noreply.github.com>
2023-11-28 11:39:41 +0800

    Delete square.js
    Delete square.js

--- square.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-    const { side } = JSON.parse(event.body);
-    const area = Math.pow(side, 2);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ shape: 'square', area }),
-    };
-};
